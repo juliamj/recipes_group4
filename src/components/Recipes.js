6012f1f88aa3c3b5c32cae22925526d6fa0dce38
Julia Jannsen <jannsen.julia@gmx.de>
2020-07-08 10:21:45 +0200

    connect react app  with contentful

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Recipe from './Recipe'
 
+//contains all the Posts contains all the posts, which it will receive from contentful
 
-const Recipes = props => {
+const Recipes = ({recipes}) => {
     return (
         <div>
-            
+            {recipes.map((article, index) => <Recipe article={article} key={index} />)}
         </div>
     )
 }
@@ -12,3 +14,5 @@ const Recipes = props => {
 
 
 export default Recipes
+
+
