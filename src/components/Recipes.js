d1a50f182db94b8c2f40c1efe4b0790c8b06babc
Madlen Engel <engel.madlen@googlemail.com>
2020-07-09 18:00:59 +0200

    Update recipes.js: Fix owl carousel

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,33 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Recipe from './Recipe';
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 //contains all the Posts, which it will receive from contentful
 
 const Recipes = ({recipes}) => {
-    console.log(recipes);
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+  setIndex(selectedIndex);
+};
+  console.log(recipes);
 
     if (recipes.length > 0) {
     return (
-        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
-            {recipes.map((article, index) =>
-               <div className='carousel-item active' key={index}>
+        <Carousel interval={null} activeIndex={index} onSelect={handleSelect}>
+            {recipes.map((article, index) => {
+              return (
+                <Carousel.Item>
+                <div key={index}>
                 <Recipe article={article} />
                 </div>
-              )}
-          </div>
-          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
+                </Carousel.Item>
+              )})}
+        </Carousel>
     )
   } else {
     return <p>Please wait...</p>
