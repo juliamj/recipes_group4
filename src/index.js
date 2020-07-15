7d1d12aef161b340edb0d473c30e2d48cd7716a2
holdenmad <holden.madagame@gmail.com>
2020-07-15 12:04:26 +0200

    Get hamburger menu to work by adding bootstrap.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.css';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';  
+// import 'bootstrap/dist/css/bootstrap.min.css';  
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 
